acb5f812f604e66b737988d67a6ce075b5cd3016
Tom Willemse <tom@ryuslash.org>
2013-07-07 23:59:02 +0200

    Add hostname keyword

--- util.lisp
+++ util.lisp
@@ -1,9 +1,8 @@
 (in-package #:scrumli)
 
-(defun start-scrumli (&key
-                        (port 8080)
+(defun start-scrumli (&key hostname (port 8080)
                         (datastore 'scrumli.pg-datastore:pg-datastore)
                         datastore-init)
   (setf *datastore* (apply #'make-instance datastore datastore-init))
   (init)
-  (start '#:scrumli :port port))
+  (start '#:scrumli :port port :hostname hostname))
